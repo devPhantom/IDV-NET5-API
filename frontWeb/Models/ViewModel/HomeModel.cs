bf44d952a7839bcbe4250742eb155f0ccddfbab1
Mlleevicky <victoria.nguyen@e-wally.com>
2019-05-30 23:32:28 +0200

    Viewmodel

---
+++ frontWeb/Models/ViewModel/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace frontWeb.Models
+{
+    public class HomeModel
+    {
+        public HomeModel()
+        {
+        }
+
+        List<Product> ProductList { get; set; }
+
+    }
+}

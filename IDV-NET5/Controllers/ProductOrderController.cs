b7b2ec185cfb304351155bd5f0dceaac76373efd
Mlleevicky <victoria.nguyen@e-wally.com>
2019-05-31 15:53:42 +0200

    productorder api

--- IDV-NET5/Controllers/ProductOrderController.cs
+++ IDV-NET5/Controllers/ProductOrderController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using IDVNET5.Models;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace IDVNET5.Controllers
 {
@@ -8,8 +10,50 @@ namespace IDVNET5.Controllers
     {
         private readonly DatabaseContext _context;
 
-        public ProductOrderController()
+        public ProductOrderController(DatabaseContext context)
         {
+            _context = context;
+        }
+
+        // GET: api/ProductOrder/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductOrder>> GetProductOrder(long id)
+        {
+            var productOrder = await _context.ProductOrders.FindAsync(id);
+
+            if (productOrder == null)
+            {
+                return NotFound();
+            }
+
+            return productOrder;
+        }
+
+        // POST:  api/ProductOrder
+        [HttpPost]
+        public async Task<ActionResult<ProductOrder>> CreateProductOrder([FromBody]ProductOrder productOrder)
+        {
+            _context.ProductOrders.Add(productOrder);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductOrder), new { id = productOrder.Id }, productOrder);
+        }
+
+        // DELETE:  api/ProductOrder/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductOrder(long id)
+        {
+            var productOrder = await _context.ProductOrders.FindAsync(id);
+
+            if (productOrder == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductOrders.Remove(productOrder);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
